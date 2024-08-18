5d0ef34b86418011e5a581b5dfebfabcf147adda
Roshan Shamas <146016164+Roshan12here@users.noreply.github.com>
2024-08-18 21:53:07 +0500

    Update CustomCards.tsx

--- app/components/CustomCards.tsx
+++ app/components/CustomCards.tsx
@@ -13,17 +13,17 @@ export default function Home() {
         <AnimatedCard 
           title="Register Account" 
           description="To register, visit our website, fill out your name, email, and password." 
-          icon={<MdLogin className="w-8 h-8 text-[#C20E4D]" />}
+          icon={<MdLogin className="w-8 h-8 text-[#331029]" />}
         />
         <AnimatedCard 
           title="Deposit Amount" 
           description="To deposit funds, log in to your account and navigate to the deposit section." 
-          icon={<RiLuggageDepositFill className="w-8 h-8 text-[#C20E4D]" />}
+          icon={<RiLuggageDepositFill className="w-8 h-8 text-[#331029]" />}
         />
         <AnimatedCard 
           title="Bitcoin Investment" 
           description="To start investing, select your desired amount and proceed with the investment options." 
-          icon={<BsCurrencyBitcoin className="w-8 h-8 text-[#C20E4D]" />}
+          icon={<BsCurrencyBitcoin className="w-8 h-8 text-[#331029]" />}
         />
       </div>
     </div>
@@ -47,7 +47,7 @@ const AnimatedCard = ({ title, description, icon }: CardProps) => {
       animate={isInView ? { opacity: 1, y: 0 } : {}}
       transition={{ duration: 0.6, ease: "easeOut" }}
       whileHover={{ scale: 1.05, y: -10 }}
-      className="relative rounded-2xl overflow-hidden bg-gradient-to-br from-[#F54180] via-[#C20E4D] to-[#920B3A] p-8 text-white shadow-lg"
+      className="relative rounded-2xl overflow-hidden bg-gradient-to-br from-[#000000] via-[#331029] to-[#310413] p-8 text-white shadow-lg"
     >
       <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-br from-transparent to-[#1a0033] opacity-60"></div>
       <div className="relative z-10">
